4cbf11d0fc95125f8d3fbd219099d8d260c822f3
Silvana Trindade <syletri@gmail.com>
2015-09-27 08:37:35 -0300

    Inserindo interface

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
-compile_server:
-	find src/main/java/ -name "*.java" -print | xargs javac -d target
+lib:
+	find src/lib -name "*.java" -print | xargs javac -d target/lib
+compile_server: lib
+	find src/server -name "*.java" -print | xargs javac -d target/server -classpath src/lib
+compile_client: lib
+	find src/client -name "*.java" -print | xargs javac -d target/client -classpath src/lib
 run_initial_server:
-	java -classpath target Main --initial
+	java -classpath target/server:target/lib Main --initial
 run_server:
-	java -classpath target Main $(ip)
+	java -classpath target/server:target/lib Main $(ip)
+run_client:
+	java -classpath target/client:target/lib Main $(ip) $(port)
---
+++ src/client/Main.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2015 Silvana Trindade and Maurício André Cinelli
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+ */
+import java.util.Scanner;
+import java.math.BigInteger;
+import yek.chord.*;
+
+class Main {
+	public static void main(String[] args) {
+	
+		if (args.length < 2) 
+		{
+			System.out.println("Wrong number of arguments");
+			System.exit(1);
+		}
+
+		NodeInfo n = null;
+
+		try {
+			int port = Integer.parseInt( args[1] );
+			Hash h = new Hash();
+			BigInteger b = h.sha1(args[0] + "/" + port);
+			n = new NodeInfo(b, args[0], port);
+
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("Node not running YEK");
+			System.exit(0);
+			return;
+		}
+
+		Scanner keyboard = new Scanner(System.in);
+		
+		while(true)
+		{	
+			System.out.println("Options:");
+			System.out.println("- get");
+			System.out.println("- put");
+			System.out.println("- delete\n");
+			System.out.print("> ");
+			String option = keyboard.next();
+			String key = "";
+
+			switch (option) 
+			{
+				case "put":
+					System.out.print("key: ");
+					key = keyboard.next();
+					System.out.print("value: ");
+					String value = keyboard.next();
+
+					Request.store(n,key,value);
+
+					break;
+				case "get":
+					System.out.print("key: ");
+					key = keyboard.next();
+
+					System.out.println(Request.get(n,key));
+					break;
+				case "delete":
+					System.out.print("key: ");
+					key = keyboard.next();
+
+					Request.delete(n,key);
+					break;
+				case "exit":
+					System.exit(1);
+				default:
+					break;		
+			}	
+		}
+	}
+}
