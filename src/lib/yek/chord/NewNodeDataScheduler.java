5105c1bda4f2c4546916435360a90d26918728c2
Silvana Trindade <syletri@gmail.com>
2015-11-10 20:53:59 -0200

    Fazendo cópia dos dados para um novo nó na rede

---
+++ src/lib/yek/chord/NewNodeDataScheduler.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2015 Silvana Trindade and Maurício André Cinelli
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+ */
+package yek.chord;
+import java.util.Enumeration;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+
+public class NewNodeDataScheduler implements Runnable {
+
+	Node node;
+	NodeInfo target;
+
+	public NewNodeDataScheduler(Node n, NodeInfo target)
+	{
+		this.node = n;
+		this.target = target;
+	}
+
+	public void run()
+	{
+		try {
+			ArrayDeque<Data> datas = new ArrayDeque<Data>();
+
+			for (Enumeration<BigInteger> it = node.hashtable.keys(); it.hasMoreElements();)
+			{
+				BigInteger key = it.nextElement();
+				Data data = node.hashtable.get(key);
+
+				if (target.id.compareTo(node.info.id) > 0)
+				{
+					if ((key.compareTo(target.id) > 0 || key.compareTo(node.info.id) < 0) )
+					{
+						datas.add(data);
+					}
+
+				} else {
+					if ((key.compareTo(target.id) > 0 && key.compareTo(node.info.id) < 0) )
+					{
+						datas.add(data);
+					}
+				}
+			}
+
+			while (datas.isEmpty() == false) {
+				Data d = datas.removeFirst();
+				Request.store(target,d.key,d.data);
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
